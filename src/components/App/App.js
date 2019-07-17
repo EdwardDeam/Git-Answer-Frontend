6339bd083bc6c0d88a6a6a69f56872ef3b9c2604
Tony Xu <Design.TXU@gmail.com>
2019-07-17 10:20:10 +1000

    fixing conflicts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,23 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import Registration from "../Registration/Registration";
-import Login from "../Login/Login";
+import PostForm from "./components/layout/PostForm";
+import CommentForm from "./components/layout/CommentForm";
+import NavBar from "./components/layout/NavBar";
+import Splash from "./components/layout/Splash";
+import Landing from "./components/layout/Landing";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <p>Hello</p>
-        <Registration />
-        <Login />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Splash />
+      <Landing />
+      <hr />
+      <PostForm />
+      <CommentForm />
+    </div>
+  );
 }
 
 export default App;
