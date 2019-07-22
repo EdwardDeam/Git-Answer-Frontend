ba54cc795cbab725daa394f3cd927e81fb8870f9
Owen <owshepherd@gmail.com>
2019-07-23 09:49:29 +1000

    Styling added for login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
+import "./Login.css";
 
 class Login extends React.Component {
   handleChange = event => {
@@ -17,19 +18,43 @@ class Login extends React.Component {
     console.log(this.props);
     if (!this.props.loggedIn) {
       return (
-        <div>
-          <h1>Login</h1>
-          <form>
-            <label>
-              Name:
-              <input onChange={this.handleChange} type="text" name="username" />
-            </label>
-            <label>
-              Password:
-              <input onChange={this.handleChange} type="text" name="password" />
-            </label>
-            <input type="submit" value="Submit" onClick={this.handleLogin} />
-          </form>
+        <div class="login-background">
+          <div class="login-box">
+            <div class="upper-labels">
+              <div class="login-signup" id="inactive">
+                <h3>Sign Up</h3>
+              </div>
+              <div class="login-login" id="active">
+                <h3>Login</h3>
+              </div>
+            </div>
+
+            <div class="signup-form">
+              <form>
+                <label for="username">Username:</label>
+                <input
+                  onChange={this.handleChange}
+                  type="text"
+                  name="username"
+                />
+
+                <label for="lname">Password:</label>
+                <input
+                  onChange={this.handleChange}
+                  type="text"
+                  name="password"
+                />
+
+                <div class="login-submit-button">
+                  <input
+                    type="submit"
+                    value="Log In"
+                    onClick={this.handleLogin}
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       );
     } else {
